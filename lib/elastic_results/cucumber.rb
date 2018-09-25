7a3613cf4ee36ab4f3b4daaf2aa11064c332d2fa
Tobias Negd <wu3rstle@gmx.de>
2018-09-25 12:22:04 +0200

    replaced cucumber event

--- lib/elastic_results/cucumber.rb
+++ lib/elastic_results/cucumber.rb
@@ -7,15 +7,15 @@ module ElasticResults
     class Formatter < ::Cucumber::Formatter::Json
       include Reportable
 
-      def after_test_case(test_case, result)
-        old_after_test_case test_case, result
+      def after_feature_element(test_case, result)
+        old_after_feature_element test_case, result
 
         feature_hash = @feature_hashes.last
         feature_hash[:elements].each do |scenario_hash|
           record_scenario feature_hash, scenario_hash
         end
       end
-      alias_method :old_after_test_case, :after_test_case
+      alias_method :old_after_feature_element, :after_feature_element
 
       def done
         ElasticResults.write_urls
